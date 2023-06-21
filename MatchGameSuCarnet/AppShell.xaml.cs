7baa4b4040bfea61b863de88fed783e730c8e8ec
canalesf23 <canalesf23@gmail.com>
2023-06-21 11:53:14 -0600

    Agregar archivos de proyecto.

---
+++ MatchGameSuCarnet/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MatchGameSuCarnet;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MatchGameSuCarnet/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MatchGameSuCarnet;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        List<string> animalEmoji = new List<string>()
+        {
+        "🐶","🐶",
+        "🙈","🙈",
+        "🦑","🦑",
+        "🐘","🐘",
+        "🦓","🦓",
+        "🦒","🦒",
+        "🐍","🐍",
+        "🐬","🐬",
+        };
+        Random random = new Random();
+        foreach (Label view in Grid1.Children)
+        {
+
+            int index = random.Next(animalEmoji.Count);
+            string nextEmoji = animalEmoji[index];
+            view.Text = nextEmoji;
+            animalEmoji.RemoveAt(index);
+
+        }
+    }
+}
+
